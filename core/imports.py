fa8cf4f670d0e4591520e8e20f4e0537230ac927
Damien Duncombe <damienduncombe@gmail.com>
2025-02-27 14:01:19 -0500

    integrated billing dashboard_tweaking functionality_6

--- core/imports.py
+++ core/imports.py
@@ -44,6 +44,7 @@ from django.contrib.auth import update_session_auth_hash
 from django.core.paginator import Paginator
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.utils.decorators import method_decorator
