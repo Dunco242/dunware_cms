6fa4f315293d0d28f71fd460c41d13308c46b4fb
Damien Duncombe <damienduncombe@gmail.com>
2025-02-27 23:42:00 -0500

    core chat issues

--- core/routing.py
+++ core/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<session_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/notifications/(?P<employee_id>\w+)/$', consumers.NotificationConsumer.as_asgi()),  # Add this line
+    re_path(r'wss/chat/(?P<session_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/notifications/(?P<employee_id>\w+)/$', consumers.NotificationConsumer.as_asgi()),  # Add this line
 
 ]
