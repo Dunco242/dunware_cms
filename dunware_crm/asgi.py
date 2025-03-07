0e8a2cd2f5c0779c7a3500a18159782a12e30131
Damien Duncombe <damienduncombe@gmail.com>
2025-03-07 17:56:53 -0500

    general notifier being integrated 8

--- dunware_crm/asgi.py
+++ dunware_crm/asgi.py
@@ -3,14 +3,17 @@ import django
 import asyncio
 import logging
 import signal
+
+
+# Ensure settings are loaded before anything else
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dunware_crm.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from core.routing import websocket_urlpatterns
 
-# Ensure settings are loaded before anything else
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dunware_crm.settings")
-django.setup()
 
 # Setup logging
 logger = logging.getLogger("django")
