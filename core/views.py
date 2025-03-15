2cfa8c56d14329c3b8c0a8cbec18424a86c8324f
Damien Duncombe <damienduncombe@gmail.com>
2025-03-15 18:24:21 -0400

    select participant was working in #11 using debug. it should be working in this one now - 24

--- core/views.py
+++ core/views.py
@@ -1538,34 +1538,20 @@ class CalendarView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        today = timezone.now().date()
 
-        try:
-            if hasattr(self.request.user, 'employee_profile'):
-                current_employee = self.request.user.employee_profile
+        # Get current employee directly - same as minimal_calendar_view
+        current_employee = self.request.user.employee_profile
 
-                # Get employees but also prepare a JSON format for JavaScript
-                available_employees = Employee.objects.exclude(id=current_employee.id).filter(is_active=True)
+        # Simple query - add employees to context
+        available_employees = list(Employee.objects.exclude(id=current_employee.id))
 
-                # Create a JSON-serializable list of employee data
-                employee_list = []
-                for emp in available_employees:
-                    employee_list.append({
-                        'id': emp.id,
-                        'name': emp.user.get_full_name() or emp.user.username,
-                        'employee_id': emp.employee_id
-                    })
-
-                # Add all data to context
-                context.update({
-                    'current_employee': current_employee,
-                    'available_employees': available_employees,
-                    'employee_list_json': json.dumps(employee_list),  # Add JSON data
-                    # ... other context data ...
-                })
-        except Exception as e:
-            # Error handling
-            pass
+        # Add to context with other required data
+        context.update({
+            'current_employee': current_employee,
+            'available_employees': available_employees,
+            'employee_count': len(available_employees),
+            'today': timezone.now().date(),
+        })
 
         return context
 
